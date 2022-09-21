3fc847a4fc57f911610063bfd67e675e3177300f
Alberto Ortega García <104846561+aortega1441@users.noreply.github.com>
2022-09-21 18:27:49 +0200

    Add files via upload

---
+++ Maquina_Golosinas2.0/src/vista/Main.java
@@ -0,0 +1,18 @@
+package vista;
+
+import modelo.MaquinaDeGolosinas;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MaquinaDeGolosinas maquinaDeGolosinas = new MaquinaDeGolosinas();
+
+	}
+
+}
