2eabca90e328275ca78967ca067acaf483134e0c
Alberto Ortega García <104846561+aortega1441@users.noreply.github.com>
2022-10-03 13:16:19 +0200

    Delete vacantes directory

--- vacantes/src/excepciones/CamposVaciosException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class CamposVaciosException extends Exception {
-
-	public CamposVaciosException(String campo) {
-		super("EL CAMPO "+campo+" NO PUEDE ESTAR VACIO");
-	}
-
-}
